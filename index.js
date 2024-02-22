4ebeb0dfc5e5e784e5dc1a850804fa1dc1390201
AlexanderSeyler <alexanderseyler@gmail.com>
2024-02-22 12:04:32 +0800

    changing port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 const axios = require("axios");
 const productRepo = require("./productRepo");
 const productRoutes = require("./routes/productRoutes");
